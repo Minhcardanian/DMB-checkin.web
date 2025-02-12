c72b5bfa54b92204551cd3b94df21b278fbf7658
Minhcadarnian <markbuicadarnian@gmail.com>
2025-02-12 10:36:51 +0700

    toggle

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,26 +1,37 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { FiChevronUp, FiChevronDown } from "react-icons/fi"; // Import icons
 import "./Header.css";
 
 function Header() {
-  const [isHidden, setIsHidden] = useState(false);
+  const [isCollapsed, setIsCollapsed] = useState(false);
 
-  const handleLogoClick = () => {
-    setIsHidden((prev) => !prev);
+  const toggleHeader = () => {
+    setIsCollapsed(!isCollapsed);
   };
 
   return (
-    <header className={`header ${isHidden ? "hidden" : ""}`}>
+    <header className={`header ${isCollapsed ? "collapsed" : ""}`}>
       <div className="header-content">
-        <Link to="/" className="logo" onClick={handleLogoClick}>
+        <Link to="/" className="logo">
           <img src="/dmblogo.jpg" alt="DMB Logo" />
         </Link>
+
         <nav className="nav-links">
           <Link to="/about">About</Link>
-          <a href="https://www.facebook.com/VGU.Dormitory" target="_blank" rel="noopener noreferrer">
+          <a
+            href="https://www.facebook.com/VGU.Dormitory"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             Facebook
           </a>
         </nav>
+
+        {/* Toggle Button */}
+        <button className="toggle-button" onClick={toggleHeader}>
+          {isCollapsed ? <FiChevronDown /> : <FiChevronUp />}
+        </button>
       </div>
     </header>
   );
